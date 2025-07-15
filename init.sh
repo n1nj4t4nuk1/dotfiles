b47bc6d5c1a6e69f7975118bd4100b95f100ad09
Javier Parada <javierparada@pm.me>
2025-07-15 10:29:11 +0200

    update prompt

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 source $DFOX_PATH/aliases/init.sh
 
-# Set fox emoji prompt
-export PS1="🦊 %~ %# "
+# Set fox emoji prompt with success/failure indicator
+setopt PROMPT_SUBST
+export PS1='🦊 <(%(?,%F{green}✓%f,%F{red}✗%f)) %~ %# '
