58b5af04fb72ce10526e785708ab6526bcd34f82
Javier Parada <javierparada@pm.me>
2026-05-01 09:12:20 +0200

    Install docker-ce, CLI, containerd, buildx and compose plugins

--- apps/susanoo/other/docker/install.sh
+++ apps/susanoo/other/docker/install.sh
@@ -2,3 +2,5 @@
 set -euo pipefail
 
 sudo dnf config-manager addrepo --from-repofile https://download.docker.com/linux/fedora/docker-ce.repo
+
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
