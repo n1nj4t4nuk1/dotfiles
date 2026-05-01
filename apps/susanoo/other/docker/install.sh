47a23509e51d0928aacb5ad768f0c915ec1455b6
Javier Parada <javierparada@pm.me>
2026-05-01 09:13:07 +0200

    Create docker group

--- apps/susanoo/other/docker/install.sh
+++ apps/susanoo/other/docker/install.sh
@@ -6,3 +6,5 @@ sudo dnf config-manager addrepo --from-repofile https://download.docker.com/linu
 sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 sudo systemctl enable --now docker
+
+sudo groupadd docker
